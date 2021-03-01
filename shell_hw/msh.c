680eb84b30a5d6f221262111c7f7b8a55b265a7b
Alex Martinez <alexander.martinez2@mavs.uta.edu>
2021-03-01 14:15:30 -0600

    Fourth Commit

--- shell_hw/msh.c
+++ shell_hw/msh.c
@@ -9,36 +9,40 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
-#define MAX_COMMAND_SIZE 255
-#define MAX_NUM_ARGUEMENTS 12
-#define WHITESPACE " \t\n"
+#define MAX_COMMAND_SIZE 100  // Limits the user to only enter 100 characters in the shell
+#define MAX_NUM_ARGUEMENTS 10 // For the user to only be able to have a maximum of 10 arguements the shell
+#define WHITESPACE " \t\n"    // Defines a whitespace as a space, tab, or enter key 
 
-int getuserinput();
 
+
+  			      // Makes sure the inital fork happens, if not the program doesnt run,
+  			      // if it does then the mshInput function runs 
 int main()
 {
   pid_t pid = fork();
   if( pid == -1)
   {
     perror("fork failed: ");
-    exit(EXIT_FAILURE);
+    _exit(1);
   }
-  else if (pid == 0)
+  else if (pid == 0)	      // Parent process
   {
-    getuserinput();
+    mshInput();
     fflush(NULL);
-    exit(EXIT_SUCCESS);
+    _exit(0);
   }
-  else
+  else			       // Child process
   {
     int status;
     waitpid(pid, &status, 0);
     fflush(NULL);
   }
-  return EXIT_SUCCESS;
+  return 0;
 }
 
-  int getuserinput()
+			      // This function shows the 'msh>' shell and reads the input the user
+			      // enters and does the appropriate action
+  int mshInput()
   {
     char *cmd_str = (char *)malloc(MAX_COMMAND_SIZE);
     while (1)
@@ -48,57 +52,65 @@ int main()
       //if (cmd_str[0] == '!' ){
         //int index = atoi(cmd_str[1]);
       //}
-      if (cmd_str[0] == '\n' || cmd_str[0] == ' ' || cmd_str[0] == '\t'){
-
-      }
+      //if (cmd_str[0] == '\n' || cmd_str[0] == ' ' || cmd_str[0] == '\t'){
+	//}
       char *token[MAX_NUM_ARGUEMENTS];
       int token_count = 0;
       char *arg_ptr;
+      
+      			      // Save a copy of the command line since strsep
+      			      // will end up miving the pointer head 
       char *working_str = strdup(cmd_str);
+      
+      			      // we are going to move the working_str pointer so
+      			      // keep track of its orginal value so we can deallocate
+      			      // the correct amount at the end
       char *working_root = working_str;
+      
+      			      // Make sure the conditions for the whitespace and number of arguements are meet
       while (((arg_ptr = strsep(&working_str, WHITESPACE)) != NULL) && (token_count < MAX_NUM_ARGUEMENTS))
       {
         token[token_count] = strndup(arg_ptr, MAX_COMMAND_SIZE);
+        		      // Implementation of the exit or quit functionality
         if (strcmp(token[token_count], "exit") == 0 || strcmp(token[token_count], "quit") == 0)
         {
-          exit(EXIT_SUCCESS);
+          exit(0);
         }
-        if (strlen(token[token_count]) == 0)
+        else if (strlen(token[token_count]) == 0)
         {
           token[token_count] = NULL;
-
         }
         token_count++;
       }
+      			        // Implementation of the valid commands the user enters
       if (strcmp(token[0],"cd") == 0)
       {
         chdir(token[1]);
         continue;
-
       }
-      //else if{}
-      //else if{}
-      else {
+      else if (strcmp(token[0], WHITESPACE) == 0)
+      {
+      	continue;
+      }
+      else 
+      {
+      				// Implementation of the invaild coomands the user enters
         pid_t pid = fork();
-        if (pid == 0)
+        if (pid == 0)		// Parent process
         {
           int ret = execvp(token[0], &token[0]);
           if (ret == -1)
           {
             printf("%s: Command not found\n", token[0]);
-            _exit(EXIT_FAILURE);
-
+            _exit(1);
           }
         }
-        else {
+        else {			// Child process
           int status;
           waitpid(pid, &status, 0);
           fflush(NULL);
         }
       }
-
     }
     return 0;
-  }
-
-    
+  }  
